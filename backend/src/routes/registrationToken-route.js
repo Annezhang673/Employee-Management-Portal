4251eb98ecdefe1dc7df0045a77b68c2bee15210
Xingcheng H <Huangxcxc@gmail.com>
2025-07-11 17:42:36 -0400

    fix style

--- backend/src/routes/registrationToken-route.js
+++ backend/src/routes/registrationToken-route.js
@@ -1,11 +1,14 @@
 import express, { Router } from "express";
-import { generateAndSendToken, getToken } from "../controllers/registration-controller.js";
+import {
+  generateAndSendToken,
+  validateToken,
+} from "../controllers/registration-controller.js";
 
 const router = Router();
 
 // HR generates token and sends email
 router.post("/generate", generateAndSendToken);
-
-router.get("/validate/:token", getToken);
+router.get("/validate/:token", validateToken);
+router.get("/status", validateToken);
 
 export default router;
