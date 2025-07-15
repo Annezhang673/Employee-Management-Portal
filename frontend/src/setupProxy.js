3ac29340a3388c452de2e20e695dcb7f25396cff
Xiaoyu Luo <xiaoyuluo0502@gmail.com>
2025-07-15 15:43:24 -0700

    Update Proxy to .env so that personal commit won't affect the package.json proxy setting.
    Fixed the problem not able to go the registration on my own side
    
    clean up code

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const target = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
+module.exports = function(app) {
+   app.use(
+      '/api',
+      createProxyMiddleware({
+         target,
+         changeOrigin: true,
+      })
+   );
+};
