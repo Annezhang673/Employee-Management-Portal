57aed8a16a0ff0087d9dd347f3001fa9a2557d1b
Xiaoyu Luo <xiaoyuluo0502@gmail.com>
2025-07-15 19:36:07 -0700

    update...

--- backend/src/routes/user-route.js
+++ backend/src/routes/user-route.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { getUserById, getUserProfile, updateUserProfile } from "../controllers/user-controller.js";
 import multer from "multer";
 import { verifyToken } from "../middlewares/authMiddleware.js";
+import { listEmployees } from "../controllers/user-controller.js";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 
 const router = Router();
 
-router.get('/', listEmployees);
+router.get('/', listEmployees); 
 
 // Get api/users/me
 router.get("/me", verifyToken, getUserProfile);
